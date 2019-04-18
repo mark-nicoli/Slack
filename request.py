b8f30e58b582ba38dc34d17ed5640b0945b22a43
Mark Nicoli <mark21198@gmail.com>
2019-04-18 16:18:10 +0100

    Update request.py

--- request.py
+++ request.py
@@ -2,18 +2,13 @@ import re
 import requests
 from slacker import Slacker
 
-#slack = Slacker('xoxp-599483118967-595997435696-599854938486-ba620468657b72088d155e8ec3bcfdf4') #test 1 authentication token
-slack = Slacker('xoxp-592981392049-587776303283-599294916432-e6bfef66c73c89f9fecbfbc931e8b25a') #test 2 authentication token
+#slack = Slacker('slack legacy token') #test 1 authentication token
+slack = Slacker('slack legacy token') #test 2 authentication token
 
 #leave channel with ID...
 #slack.channels.leave('CHMR17JMC')
 
-'''list members of slack workspace and get their ID from their username
-    ali ID - DHKMJJYSU
-    emmet ID - DHKM3EEG0
-    me - UHHVBCTLG
-    slack.channels.kick('CHLB8SN4C','DHKMJJYSU')
-    update(8/4/2019): keep Steve in certain extpat channels'''
+'''list members of slack workspace and get their ID from their username'''
 
 # Get users list
 response = slack.users.list()
@@ -31,13 +26,13 @@ print()
 
 
 proxy_endpoint = 'http://myproxy:3128'
-slack = Slacker('xoxp-592981392049-587776303283-599294916432-e6bfef66c73c89f9fecbfbc931e8b25a',
+slack = Slacker('slack legacy token',
                 http_proxy=proxy_endpoint,
                 https_proxy=proxy_endpoint)
 
 from requests.sessions import Session
 
-token = Slacker('xoxp-592981392049-587776303283-599294916432-e6bfef66c73c89f9fecbfbc931e8b25a')
+token = Slacker('slack legacy token')
 
 with Session() as session:
     slack = Slacker(token, session=session)
