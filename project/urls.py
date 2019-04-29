9bc6f1bfe263bef2e8903ad3722e35c9ea121861
Mark Nicoli <mark21198@gmail.com>
2019-04-29 14:09:37 +0100

    Add files via upload

---
+++ project/urls.py
@@ -0,0 +1,28 @@
+"""remove URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from slacky import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^select/', views.select, name ='select'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^output/', views.output, name='output'),
+    url(r'^$', views.signup, name='signup'),
+    url(r'^index/', views.index, name='index'),
+]
